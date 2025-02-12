3872e7b70bc80b81b9638c4c3af8f1f84e0e9c11
LimitadosDoPc <apenobru451@gmail.com>
2025-02-12 10:36:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const questions = document.querySelectorAll('.faq-question');
+
+questions.forEach(question => {
+    question.addEventListener('click', () => {
+        const faqItem = question.parentElement;
+        faqItem.classList.toggle('active');
+    });
+});
+
+
+
+// Servir arquivos estáticos (o frontend HTML)
+app.use(express.static('public'));
+
+// Iniciar o servidor
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
+
+try {
+
+} catch (error) {
+    console.error("Erro encontrado:", error);
+}
+
+<script src="script.js" defer></script>
